dbfb18aef71e33d5d6047162bfd92d4f0f926815
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-08-09 22:37:25 +0900

    TODOセーブ画面の簡易実装

---
+++ todo-front/.nuxt/router.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _c5d3819a = () => interopDefault(import('../pages/inspire.vue' /* webpackChunkName: "pages/inspire" */))
+const _19ac9318 = () => interopDefault(import('../pages/save.vue' /* webpackChunkName: "pages/save" */))
+const _3991f56b = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/inspire",
+    component: _c5d3819a,
+    name: "inspire"
+  }, {
+    path: "/save",
+    component: _19ac9318,
+    name: "save"
+  }, {
+    path: "/",
+    component: _3991f56b,
+    name: "index"
+  }, {
+    path: "/:p",
+    component: _c5d3819a,
+    name: "contents"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
---
+++ todo-front/pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <v-container class="grey lighten-5">
+      <v-row no-gutters>
+        <v-col cols="12" sm="12" md="12">
+          <v-card v-for="(item, index) in items" :key="index">
+            <div
+              class="d-flex flex-no-wrap justify-space-between"
+            >
+              <nuxt-link :to="'/' + item.todono">
+                <div>
+                  <v-card-title class="text-h5" v-text="item.name">
+                  </v-card-title>
+                  <v-card-subtitle
+                    v-text="item.content"
+                  ></v-card-subtitle>
+                </div>
+              </nuxt-link>
+            </div>
+          </v-card>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      items: [
+        {},
+      ],
+    };
+  },
+  mounted() {
+    axios
+      .get("/api/get_all")
+      .then((response) => {
+        console.log(response);
+        this.items = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
+<style lang="scss">
+
+</style>
