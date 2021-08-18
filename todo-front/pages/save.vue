c68828c1601acb1c56d9900de09ad0939eb63a08
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-08-18 19:42:00 +0900

    form部分をコンポーネント化

--- todo-front/pages/save.vue
+++ todo-front/pages/save.vue
@@ -1,124 +1,13 @@
-
-  <template>
-  <validation-observer ref="observer" v-slot="{ invalid }">
-    <form @submit.prevent="submit">
-      <validation-provider
-        v-slot="{ errors }"
-        name="Name"
-        rules="required|max:10"
-      >
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          :error-messages="errors"
-          label="Name"
-          required
-        ></v-text-field>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        name="Content"
-        rules="required|max:100"
-      >
-        <v-text-field
-          v-model="content"
-          :counter="10"
-          :error-messages="errors"
-          label="Content"
-          required
-        ></v-text-field>
-      </validation-provider>
-
-      <Dialog :invalid="invalid" />
-      <SchedulePicker @schedule="emitScheduleData" />
-      <p>{{ startDate }}</p>
-      <p>{{ endDate }}</p>
-
-      <v-btn @click="clear"> clear </v-btn>
-    </form>
-  </validation-observer>
+<template>
+<SaveForm />
 </template>
 <script>
-import axios from "axios";
-import { required, digits, email, max, regex } from "vee-validate/dist/rules";
-import {
-  extend,
-  ValidationObserver,
-  ValidationProvider,
-  setInteractionMode,
-} from "vee-validate";
+import { Component } from "preact";
+import SaveForm from "~/components/save/ui/save-form.vue";
 
-import Dialog from "~/components/save/ui/dialog.vue";
-import SchedulePicker from "~/components/save/ui/schedule-picker.vue";
-
-setInteractionMode("eager");
-
-extend("digits", {
-  ...digits,
-  message: "{_field_} needs to be {length} digits. ({_value_})",
-});
-
-extend("required", {
-  ...required,
-  message: "{_field_} can not be empty",
-});
-
-extend("max", {
-  ...max,
-  message: "{_field_} may not be greater than {length} characters",
-});
-
-extend("regex", {
-  ...regex,
-  message: "{_field_} {_value_} does not match {regex}",
-});
 export default {
-  components: {
-    ValidationProvider,
-    ValidationObserver,
-    Dialog,
-    SchedulePicker,
-  },
-
-  data() {
-    return {
-      name: "",
-      content: "",
-      msg: "",
-      
-      startDate: "",
-      endDate: "",
-      
-    };
-  },
-  methods: {
-    submit() {
-      this.$refs.observer.validate();
-      axios
-        .post("/v1/api/todo/save", {
-          name: this.name,
-          content: this.content,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      this.clear();
-    },
-    clear() {
-      this.name = "";
-      this.content = "";
-      this.$refs.observer.reset();
-    },
-    emitScheduleData(...sheduleData) {
-      console.log(sheduleData);
-      this.startDate = sheduleData[0];
-      this.endDate = sheduleData[1];
-    },
-  },
-};
+  component: {
+    SaveForm,
+  }
+}
 </script>
-<style lang="scss">
-</style>
