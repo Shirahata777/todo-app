ebbb4ddf64f49264820a8446cb125a6f7eca6d07
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-08-19 19:56:53 +0900

    ファイル削除

--- mysql/sql/schedule.sql
+++
@@ -1,7 +0,0 @@
-CREATE SCHEMA todo_db; 
-USE todo_db;
-
-DROP TABLE IF EXISTS schedule;
-CREATE TABLE schedule ( scheduleno SERIAL NOT NULL, todono INT NOT NULL, start_day VARCHAR(255) NOT NULL, end_day VARCHAR(255) NOT NULL, PRIMARY KEY (scheduleno) );
-
-INSERT INTO schedule (todono, start_day, end_day) VALUES (1, "2021-09-01", "2021-09-02");
--- mysql/sql/todo.sql
+++
@@ -1,6 +0,0 @@
-USE todo_db;
-
-DROP TABLE IF EXISTS todo;
-CREATE TABLE todo ( todono SERIAL NOT NULL, userno INT NOT NULL, title VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, PRIMARY KEY (todono) );
-
-INSERT INTO todo (name, title, content) VALUES ("テストタイトル", "テストデータ");
