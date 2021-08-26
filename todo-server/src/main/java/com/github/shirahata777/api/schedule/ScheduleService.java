7a1c6568a9a47dae5d6ecb7f201ce824402c5e08
Shirahata777 <y_shirahata_181@yahoo.co.jp>
2021-08-26 20:48:30 +0900

    スケージュール詳細API実装

--- todo-server/src/main/java/com/github/shirahata777/api/schedule/ScheduleService.java
+++ todo-server/src/main/java/com/github/shirahata777/api/schedule/ScheduleService.java
@@ -1,13 +1,9 @@
 package com.github.shirahata777.api.schedule;
 
+import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import javax.json.JsonObject;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.hibernate.LockMode;
@@ -15,13 +11,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.github.shirahata777.api.todo.TodoService;
 import com.github.shirahata777.dao.table.todo.ScheduleTable;
-import com.github.shirahata777.dao.table.todo.TodoTable;
 import com.github.shirahata777.entity.SaveData;
 
 import io.helidon.webserver.Routing;
@@ -31,11 +24,12 @@ import io.helidon.webserver.Service;
 
 public class ScheduleService implements Service {
 
-	private static Logger log = LoggerFactory.getLogger(TodoService.class);
+	private static Logger log = LoggerFactory.getLogger(ScheduleService.class);
 
 	@Override
 	public void update(Routing.Rules rules) {
-		rules.get("/schedule", this::serachScheduleHandler);
+		rules.get("/schedule", this::serachScheduleHandler).get("/schedule/{scheduleNo}/detail",
+				this::getDetailScheduleDataHandler);
 	}
 
 	public void saveScheduleDataHandler(ServerRequest request, ServerResponse response) {
@@ -51,64 +45,40 @@ public class ScheduleService implements Service {
 
 	private void serachScheduleHandler(ServerRequest request, ServerResponse response) {
 
-		Configuration cfg = null;
-		SessionFactory sessionFactory = null;
-		Session session = null;
-		Transaction transaction = null;
+		Configuration cfg = new Configuration().configure();
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		String queryText = "select * from schedule s";
+		List allQuery = session.createSQLQuery(queryText).addEntity("s", ScheduleTable.class).list();
+
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonString = null;
 		try {
-
-			// 構成情報の読み込み
-			cfg = new Configuration().configure();
-			// セッションファクトリをビルド
-			sessionFactory = cfg.buildSessionFactory();
-			// セッションを取得
-			session = sessionFactory.openSession();
-
-			String queryText = "select * from todo t left join schedule s on (t.todono=s.todono)";
-			List allQuery = session.createSQLQuery(queryText)
-					.addEntity("t", TodoTable.class)
-					.addEntity("s", ScheduleTable.class)
-					.list();
-
-			ObjectMapper mapper = new ObjectMapper();
-			String jsonString = mapper.writeValueAsString(allQuery);
-
-			response.send(jsonString);
-		} catch (Exception e) {
-			if (transaction != null) {
-				log.warn(e.toString());
-				transaction.rollback();
-			}
-			e.printStackTrace();
-			response.send("No Get Data!");
-		} finally {
-			if (session != null) {
-				session.close();
-			}
+			jsonString = mapper.writeValueAsString(allQuery);
+		} catch (IOException e) {
+			log.warn(e.toString());
 		}
+
+		response.send(jsonString);
 
 	}
 
-	// TODO 作成中
-	public void getDetailFormDataHandler(ServerRequest request, ServerResponse response) {
+	public void getDetailScheduleDataHandler(ServerRequest request, ServerResponse response) {
 		Configuration cfg = null;
 		SessionFactory sessionFactory = null;
 		Session session = null;
 		Transaction transaction = null;
 
-		int todoNo = Integer.parseInt(request.path().param("todoNo"));
+		int scheduleNo = Integer.parseInt(request.path().param("scheduleNo"));
 		try {
 
-			// 構成情報の読み込み
 			cfg = new Configuration().configure();
-			// セッションファクトリをビルド
 			sessionFactory = cfg.buildSessionFactory();
-			// セッションを取得
 			session = sessionFactory.openSession();
-			// トランザクションを開始
 			transaction = session.beginTransaction();
+
 			LockMode lockMode = LockMode.NONE;
-			TodoTable detailQuery = session.get(TodoTable.class, todoNo, lockMode);
+			ScheduleTable detailQuery = session.get(ScheduleTable.class, scheduleNo, lockMode);
 
 			ObjectMapper mapper = new ObjectMapper();
 			String jsonString = mapper.writeValueAsString(detailQuery);
@@ -118,7 +88,7 @@ public class ScheduleService implements Service {
 			if (transaction != null) {
 				transaction.rollback();
 			}
-			e.printStackTrace();
+			log.warn(e.toString());
 			response.send("No Get Data!");
 		} finally {
 			if (session != null) {
